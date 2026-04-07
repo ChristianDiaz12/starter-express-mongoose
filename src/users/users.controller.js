e621c2912e1a95cf022bc79f1f5cc45426e77f44
ChristianDiaz12 <90482847+ChristianDiaz12@users.noreply.github.com>
2026-04-07 12:00:47 -0400

    Update users.controller.js

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,9 +1,26 @@
 const User = require("./users.model")
 
 async function list(req, res) {
-  const users = await User.find();
+  const users = await User.find()
   res.send(users);
 }
+
+async function userExists(req, res, next) {
+  const { userId } = req.params;
+  const foundUser = await User.findOne({"_id":userId});
+  if (foundUser) {
+    res.locals.user = foundUser;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `User id not found: ${userId}`,
+  });
+};
+
+function read(req, res, next) {
+  res.json({ data: res.locals.user });
+};
 
 function bodyDataHas(propertyName) {
   return function (req, res, next) {
@@ -13,7 +30,7 @@ function bodyDataHas(propertyName) {
     }
     next({
         status: 400,
-        message: 'Must include a ${propertyName}'
+        message: `Must include a ${propertyName}`
     });
   };
 }
@@ -24,15 +41,41 @@ async function create(req, res) {
       username: username,
       email: email,
   })
-  await newUser.save();
+  await newUser.save()
   res.status(201).json({ data: newUser });
+}
+
+async function update(req, res) {
+  const user = res.locals.user;
+  const { data: { username, email } = {} } = req.body;
+
+  user.username = username;
+  user.email = email;
+  await user.save()
+
+  res.json({ data: user });
+}
+
+async function destroy(req, res) {
+  const { userId } = req.params;
+  await User.deleteOne({ _id: userId })
+  res.sendStatus(204);
 }
 
 module.exports = {
   list,
+  read: [userExists, read],
   create: [
       bodyDataHas("username"),
       bodyDataHas("email"),
       create
-  ]
+  ],
+  update: [
+      userExists,
+      bodyDataHas("username"),
+      bodyDataHas("email"),
+      update
+  ],
+  delete: [userExists, destroy],
+  userExists,
 };
